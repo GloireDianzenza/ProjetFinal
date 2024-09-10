46bb7ffce531cf6155d6e290a59d9cb2600a4250
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-09-10 12:44:02 +0200

    admin

---
+++ final-project/frontend/js/edit_post_admin.js
@@ -0,0 +1,66 @@
+const url = new URL(window.location);
+const searchParam = url.searchParams;
+const id = searchParam.get("id");
+const postID = searchParam.get("post");
+if(id == null || id.trim() == '' || postID == null || postID.trim() == '')window.location = "index.html";
+let currentUser,currentPost;
+const re = /(?:\.([^.]+))?$/;
+
+async function define(){
+    let request = await fetch("http://localhost:3500/api/user/user/"+id);
+    let data = await request.json();
+    request = await fetch("http://localhost:3500/api/post/"+postID);
+    let data2 = await request.json();
+    request = await fetch("http://localhost:3500/api/user/admin");
+    let admins = await request.json();
+    for(let a of admins){
+        if(a.id === data.id)return [data,data2];
+    }
+    return [{error:true},data2];
+}
+
+define().then(lst=>{
+    currentUser = lst[0];
+    currentPost = lst[1];
+})
+.then(()=>{
+    console.log(currentUser,currentPost);
+    if(currentUser.error || currentPost.error){
+        window.location = "index.html";
+    }
+    txtInput.value = currentPost.texte;
+    imgInput.value = currentPost.image;
+    previewImg.src = imgInput.value
+});
+
+logout.addEventListener("click",()=>{
+    fetch("http://localhost:3500/api/user/reset",{
+        method:"PUT",
+        headers:{
+            "Content-Type":"application/json",
+            'Accept':"application/json",
+        }
+    })
+    .then(response=>response.json())
+    .then(data=>{
+        console.log(data);
+        window.location = "index.html";
+    })
+});
+
+document.getElementById("return").addEventListener("click",()=>{
+    window.location = "./your_posts.html?id="+id;
+})
+
+let form = document.querySelector("form");
+let submitter = document.querySelector("input[type=submit]");
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+});
+
+previewBtn.addEventListener("click",()=>{
+    let ext = re.exec(imgInput.value)[1];
+    if(ext === undefined || ext !== "gif")return;
+    previewImg.src = imgInput.value;
+})
--- final-project/frontend/js/main_admin.js
+++ final-project/frontend/js/main_admin.js
@@ -42,5 +42,49 @@ logout.addEventListener("click",()=>{
 })
 
 async function getPosts() {
-    console.log("ss");
+    fetch("http://localhost:3500/api/post/")
+    .then(response=>response.json())
+    .then(async data=>{
+        console.log(data);
+        for(let post of data){
+            let request1 = await fetch("http://localhost:3500/api/user/user/"+post.UserId);
+            let dataUser = await request1.json();
+
+            let request2 = await fetch("http://localhost:3500/api/comment/list",{method:"POST",headers:{"Content-Type":"application/json","Accept":"application/json",},body:JSON.stringify({id:post.id})});
+            let dataComment = await request2.json();
+            console.log(dataComment);
+            let commentHTML = "";
+            for(let comment of dataComment){
+                
+                let request3 = await fetch("http://localhost:3500/api/user/user/"+comment.UserId);
+                let commentUser = await request3.json();
+
+                commentHTML += `
+                    <div class="comment flex flex-col justify-around items-center">
+                            <h2 class="text-left">${commentUser.email}</h2>
+                            <p class="text-xs">${comment.value}</p>
+                    </div>
+                `;
+            }
+
+            let newPost = `
+                <div class="post p-3 shadow-xl rounded-lg border-black border border-opacity-10 min-w-32 text-center flex flex-col gap-4">
+                    <h2 class="text-left">${dataUser.email}</h2>
+                    <strong class="text-center">${post.date}</strong>
+                    <img src="${post.image}" alt="">
+                    <p class="text-left">${post.texte}</p>
+                    <div class="comments flex flex-col justify-start items-center overflow-x-hidden overflow-y-scroll max-h-32 gap-2">
+                        ${commentHTML}
+                    </div>
+                    <div class="buttons flex justify-center items-center text-xs gap-2">
+                        <button type="button" class="bg-yellow-300 p-2 rounded-md hover:font-bold hover:bg-white transition duration-300 ease-in-out">Comment</button>
+                        <a href="edit_post_admin.html"><button class="editBtn bg-green-300 p-1.5 rounded-lg hover:font-bold hover:bg-white transition duration-300 ease-in-out">Modifier post</button></a>
+                        <button class="deleteBtn bg-red-300 p-1.5 rounded-lg hover:font-bold hover:bg-white transition duration-300 ease-in-out">Supprimer post</button>
+                    </div>
+                </div>
+            `;
+            document.querySelector(".posts").innerHTML += newPost;
+        }
+    })
+    .catch(error=>console.error("Error: ",error));
 }
