70e35137aa8333780f4c006e1899148e41439953
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-11-22 11:02:12 +0100

    model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,7 +2,9 @@ const {sequelize,DataTypes} = require("../init");
 
 const User = sequelize.define("User",{
     id:{type:DataTypes.INTEGER,allowNull:false,primaryKey:true,autoIncrement:true},
-    email:{type:DataTypes.STRING,allowNull:false,unique:true},
+    email:{type:DataTypes.STRING,allowNull:false,unique:true,validate:{
+        isEmail:true
+    }},
     password:{type:DataTypes.STRING,allowNull:false},
     admin:{type:DataTypes.TINYINT,allowNull:false,defaultValue:0}
 });
